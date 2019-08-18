325658291bada4b26bacf2cf028ca16458d1b5d4
dotkom <dotkom@dotkom-ideacentre>
2019-08-19 03:39:42 +0700

    new tiket concept on process update db mysql

--- antrian.sql
+++ antrian.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.6deb5
+-- version 4.8.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: 16 Agu 2019 pada 20.49
--- Versi Server: 5.7.27-0ubuntu0.18.04.1
--- PHP Version: 7.2.19-0ubuntu0.18.04.1
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 19 Agu 2019 pada 03.38
+-- Versi server: 10.1.31-MariaDB
+-- Versi PHP: 5.6.35
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -30,7 +32,8 @@ CREATE TABLE `antrian` (
   `id` int(11) NOT NULL,
   `tgl` date NOT NULL,
   `urut` smallint(4) NOT NULL,
-  `loket` smallint(2) NOT NULL,
+  `tiket` smallint(2) NOT NULL,
+  `loket` smallint(2) DEFAULT NULL,
   `status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -38,9 +41,61 @@ CREATE TABLE `antrian` (
 -- Dumping data untuk tabel `antrian`
 --
 
-INSERT INTO `antrian` (`id`, `tgl`, `urut`, `loket`, `status`) VALUES
-(1, '2019-08-14', 1, 1, 0),
-(2, '2019-08-16', 1, 0, 3);
+INSERT INTO `antrian` (`id`, `tgl`, `urut`, `tiket`, `loket`, `status`) VALUES
+(1, '2019-08-19', 1, 0, 1, 0),
+(2, '2019-08-19', 1, 0, 2, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `loket`
+--
+
+CREATE TABLE `loket` (
+  `id` smallint(2) NOT NULL,
+  `nmloket` varchar(30) NOT NULL,
+  `ket` varchar(30) NOT NULL,
+  `sound` varchar(30) DEFAULT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `loket`
+--
+
+INSERT INTO `loket` (`id`, `nmloket`, `ket`, `sound`, `status`) VALUES
+(1, 'LOKET CS', 'Customer Service', 'loketcs', 1),
+(2, 'LOKET 1', 'Antrian  Loket 1', 'loket1', 1),
+(3, 'LOKET 2', 'Antrian  Loket 2', 'loket2', 1),
+(4, 'LOKET 3', 'Antrian  Loket 3', 'loket3', 1),
+(5, 'LOKET 4', 'Antrian  Loket 4', 'loket4', 1),
+(6, 'LOKET 5', 'Antrian  Loket 5', 'loket5', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tiket`
+--
+
+CREATE TABLE `tiket` (
+  `id` smallint(2) NOT NULL,
+  `nmtiket` varchar(30) NOT NULL,
+  `kdhuruf` varchar(2) NOT NULL,
+  `ket` varchar(30) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tiket`
+--
+
+INSERT INTO `tiket` (`id`, `nmtiket`, `kdhuruf`, `ket`, `status`) VALUES
+(1, 'Tiket Antrian CS', 'A', 'Tiket Antrian CS', 1),
+(2, 'Tiket Antrian 1', 'B', 'Tiket Antrian 1', 1),
+(3, 'Tiket Antrian 2', 'C', 'Tiket Antrian 2', 1),
+(4, 'Tiket Antrian 3', 'D', 'Tiket Antrian 3', 1),
+(5, 'Tiket Antrian 4', 'E', 'Tiket Antrian 4', 1),
+(6, 'Tiket Antrian 5', 'F', 'Tiket Antrian 5', 1);
 
 -- --------------------------------------------------------
 
@@ -64,40 +119,67 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `nama`, `username`, `password`, `loket`, `tipe`, `status`, `lastlog`) VALUES
-(1, 'Customer Service 1', 'cs1', '$2y$10$gN5ZqHZQTeEEneBbUft./OIR4tD0kUW8EznSrtDuhHB.5uvyo7OKS', 0, 1, 1, '2019-08-16 03:02:25'),
-(2, 'Customer Service 2', 'cs2', '$2y$10$.wFXxlfz4614MFE9J22i..90PsgPUaXAE7FLO5K3MaVAbQMZY8Z.a', 0, 1, 1, '2019-08-13 00:00:00'),
-(3, 'Administrator', 'admin', '$2y$10$.wFXxlfz4614MFE9J22i..90PsgPUaXAE7FLO5K3MaVAbQMZY8Z.a', 0, 0, 1, '2019-08-16 01:33:19');
+(1, 'Customer Service 1', 'cs1', '$2y$10$gN5ZqHZQTeEEneBbUft./OIR4tD0kUW8EznSrtDuhHB.5uvyo7OKS', 1, 1, 1, '2019-08-16 03:02:25'),
+(2, 'Customer Service 2', 'cs2', '$2y$10$.wFXxlfz4614MFE9J22i..90PsgPUaXAE7FLO5K3MaVAbQMZY8Z.a', 1, 1, 1, '2019-08-13 00:00:00'),
+(3, 'Administrator', 'admin', '$2y$10$.wFXxlfz4614MFE9J22i..90PsgPUaXAE7FLO5K3MaVAbQMZY8Z.a', 0, 0, 1, '2019-08-19 02:09:14');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `antrian`
+-- Indeks untuk tabel `antrian`
 --
 ALTER TABLE `antrian`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `users`
+-- Indeks untuk tabel `loket`
+--
+ALTER TABLE `loket`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tiket`
+--
+ALTER TABLE `tiket`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `antrian`
+-- AUTO_INCREMENT untuk tabel `antrian`
 --
 ALTER TABLE `antrian`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT untuk tabel `loket`
+--
+ALTER TABLE `loket`
+  MODIFY `id` smallint(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `tiket`
+--
+ALTER TABLE `tiket`
+  MODIFY `id` smallint(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
